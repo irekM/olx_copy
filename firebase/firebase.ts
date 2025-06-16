c121ff2173a53de39aecbaa939c1b17839e3daa9
IrekM <12935670+irekM@users.noreply.github.com>
2025-06-17 00:22:48 +0200

    add and finish firebase configuration

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -5,16 +5,19 @@ import { getAuth } from 'firebase/auth';
 
 // configuration from Firebase Console
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: 'AIzaSyBmyBrwpDkq95Mp8dSn_jC2JH3h3NZoh9Y',
+    authDomain: 'olx-copy-ccb9f.firebaseapp.com',
+    projectId: 'olx-copy-ccb9f',
+    storageBucket: 'olx-copy-ccb9f.firebasestorage.app',
+    messagingSenderId: '562918629200',
+    appId: '1:562918629200:web:e4438032e2ad873a115655',
+    measurementId: 'G-51BMJP48Z7',
 };
 
-// anti-pattern: avoid using `initializeApp` multiple times
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// // anti-pattern: avoid using `initializeApp` multiple times
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
